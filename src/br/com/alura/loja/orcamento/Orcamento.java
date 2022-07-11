cfbb0a3c4e5d5802af524fef54d785bac2d81415
whenes <whenes.oliveira@gmail.com>
2022-07-11 16:51:00 -0300

    Aplicado o pattern State

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,29 +1,36 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
-    private String situacao;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
     public void aplicarDescontoExtra() {
-        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
-        if (situacao.equalsIgnoreCase("EM_ANALISE")) {
-            valorDoDescontoExtra = new BigDecimal("0.05");
-        } else if (situacao.equalsIgnoreCase("APROVADO")) {
-            valorDoDescontoExtra = new BigDecimal("0.02");
-        }
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
         this.valor = this.valor.subtract(valorDoDescontoExtra);
     }
 
     public void aprovar() {
-        this.situacao = "APROVADO";
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
     }
 
     public BigDecimal getValor() {
@@ -32,5 +39,13 @@ public class Orcamento {
 
     public int getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 }
---
+++ src/br/com/alura/loja/orcamento/situacao/Finalizado.java
@@ -0,0 +1,7 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class Finalizado extends SituacaoOrcamento {}
