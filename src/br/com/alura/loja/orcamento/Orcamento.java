7b36f56dac63badaf26932d9f8a3c74aa83c0b54
whenes <whenes.oliveira@gmail.com>
2022-07-11 16:02:39 -0300

    Criado o cenário para o pattern state

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -5,10 +5,25 @@ import java.math.BigDecimal;
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+        if (situacao.equalsIgnoreCase("EM_ANALISE")) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if (situacao.equalsIgnoreCase("APROVADO")) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 
     public BigDecimal getValor() {
