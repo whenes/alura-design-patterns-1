52477230e2d6aa1bdad40e3f513fb434f7251113
whenes <whenes.oliveira@gmail.com>
2022-07-11 15:56:03 -0300

    Aplicado o pattern Template Method

--- src/br/com/alura/loja/desconto/chainofresponsibility/Desconto.java
+++ src/br/com/alura/loja/desconto/chainofresponsibility/Desconto.java
@@ -11,5 +11,12 @@ public abstract class Desconto {
         this.proximo = proximo;
     }
 
-    public abstract BigDecimal calcular(Orcamento orcamento);
+    public BigDecimal calcular(Orcamento orcamento) {
+        if (deveAplicar(orcamento)) {
+            return efetuarCalculo(orcamento);
+        }
+        return proximo.efetuarCalculo(orcamento);
+    }
+    public abstract BigDecimal efetuarCalculo(Orcamento orcamento);
+    protected abstract boolean deveAplicar(Orcamento orcamento);
 }
